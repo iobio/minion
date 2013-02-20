7ad5a873cd75804d062796ad9f2f56905b7a1ba6
Chase Miller <chmille4@gmail.com>
2013-02-20 05:52:58 -0500

    Fix bug where data would be lost when multiple calls got sent to freebayes send() function at the same time

--- freebayesServer.js
+++ freebayesServer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 // temporary until i understand why freebayes seg faults on the stream
-// process.on('uncaughtException', function (exception) {
-//    // handle or ignore error
-// });
+process.on('uncaughtException', function (exception) {
+   // handle or ignore error
+});
 
 var minion = require('./minion'),
     http = require('http'),
@@ -22,28 +22,27 @@ var tool = {
    path: '/Users/chase/Tools/freebayes/bin/freebayes',
    options: ['--stdin'],
    send : function(data) {
-      data = String(data)
+      data = String(data);      
       var lines = data.split("\n");
       var numLines = lines.length;
+      var results = [];
       for (var i=0; i < numLines; i++) {
-         data = lines[i];  
-         if( !data )  continue;
-         if (data.charAt(0) != '#') {
-            var values = data.split("\t");
-            data = {
-               chrom : values[0],
-               pos   : values[1],
-               id    : values[2],
-               ref   : values[3],
-               alt   : values[4],
-               qual  : values[5],
-               filter: values[6],
-               info  : values[7]
-            };
-            //if (data) socket.emit( 'results', { data : data });
-            return data;
+         var line = lines[i];
+         if (line && line.charAt(0) != '#') {
+            var values = line.split("\t");
+            results.push ({
+                chrom : values[0],
+                pos   : values[1],
+                id    : values[2],
+                ref   : values[3],
+                alt   : values[4],
+                qual  : values[5],
+                filter: values[6],
+                info  : values[7]
+            });
          }
       }
+      return JSON.stringify(results);
    }
 };
 
