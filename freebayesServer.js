60086253740af90cf072a242ebf17c7ef68ec005
Chase Miller <chmille4@gmail.com>
2013-03-29 10:56:13 -0400

    Added example url in comment header

--- freebayesServer.js
+++ freebayesServer.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+// freebayesServer.js
+// example url: curl "http://0.0.0.0:8080/?cmd=-f%20/Users/chase/Tools/freebayes/data/hs_ref_chr11.fa%20http://0.0.0.0:8030?cmd=11:1000000-1050000%2520'http://s3.amazonaws.com/1000genomes/data/NA06984/alignment/NA06984.chrom11.ILLUMINA.bwa.CEU.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA06985/alignment/NA06985.chrom11.ILLUMINA.bwa.CEU.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA06986/alignment/NA06986.chrom11.ILLUMINA.bwa.CEU.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA06989/alignment/NA06989.chrom11.ILLUMINA.bwa.CEU.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18486/alignment/NA18486.chrom11.ILLUMINA.bwa.YRI.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18487/alignment/NA18487.chrom11.ILLUMINA.bwa.YRI.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18488/alignment/NA18488.chrom11.ILLUMINA.bwa.YRI.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18489/alignment/NA18489.chrom11.ILLUMINA.bwa.YRI.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18525/alignment/NA18525.chrom11.SOLID.bfast.CHB.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18526/alignment/NA18526.chrom11.ILLUMINA.bwa.CHB.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18527/alignment/NA18527.chrom11.SOLID.bfast.CHB.low_coverage.20111114.bam'%2520'http://s3.amazonaws.com/1000genomes/data/NA18528/alignment/NA18528.chrom11.SOLID.bfast.CHB.low_coverage.20111114.bam'"
+// example websocket: socket.emit('run', { 'url' : url })
+
+
 // temporary until i understand why freebayes seg faults on the stream
 process.on('uncaughtException', function (exception) {
    // handle or ignore error
