123076c8627bf0a0967754ffdf956c635324394a
Chase Miller <chmille4@gmail.com>
2013-06-03 14:25:01 -0400

    move hashbang to line 1

--- services/bamtoolsServer.js
+++ services/bamtoolsServer.js
@@ -1,9 +1,9 @@
+#!/usr/bin/env node
 // Chase Miller 2013
 // uses helper script to combine samtools and bamtools to 
 // grab a region of mulitple bam files and merge
 // samtools is used b\c it is quicker than bamtools at remote slices
 
-#!/usr/bin/env node
 
 // initialize server
 var minion = require('../minion'),
