bdecae7db770c7f70014dfbcf9937a9edc3beca3
Chase Miller <chmille4@gmail.com>
2013-06-06 11:53:52 -0400

    reorganize and clean up

--- helpers/bamBinnerHelper.sh
+++ helpers/bamBinnerHelper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bamtools="/Users/chase/Tools/qbamtools/bin/qbamtools"
+bamtools="bamtools"
 region=$1;
 
 #/Users/chase/Tools/qbamtools/bin/qbamtools convert -format json -in <(samtools view -u http://s3.amazonaws.com/1000genomes/data/HG00096/alignment/HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20111114.bam 11:108473-188673)
