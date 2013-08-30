73189d5fa090afee45e4f71e80dca5b29eff28b2
Chase Miller <chmille4@gmail.com>
2013-08-30 02:53:02 -0400

    add option to determine number of bins

--- bin/bamBinnerHelper.sh
+++ bin/bamBinnerHelper.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+numBins=100
+while getopts n: flag; do
+  case $flag in
+    n)
+      numBins=$OPTARG
+      ;;
+    ?)
+      exit;
+      ;;
+  esac
+done
+
+shift $(( OPTIND - 1 ));
+
 bamtools="bamtools"
 region=$1;
 
@@ -17,6 +31,6 @@ done
 min=$(echo $region | cut  -d: -f2 | cut -d- -f1)
 max=$(echo $region | cut  -d: -f2 | cut -d- -f2)
 
-cmd=$bamtools" merge "$cmd" | "$bamtools" coverage | cut -f3 | $DIR/../bin/binner.sh $min $max 100"
+cmd=$bamtools" merge "$cmd" | "$bamtools" coverage | cut -f3 | $DIR/../bin/binner.sh $min $max $numBins"
 eval $cmd
 
