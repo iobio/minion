0d3773a1fcf320d2a20b3a6d6e24e4069ccc2e46
Chase Miller <chmille4@gmail.com>
2013-09-27 09:43:08 -0400

    change bam stream to compressed

--- bin/bamHelper.sh
+++ bin/bamHelper.sh
@@ -8,7 +8,7 @@ cmd=""
 
 for i in ${@:2}
 do   
-   cmd=$cmd" -in <(samtools view -u $i $region)"
+   cmd=$cmd" -in <(samtools view -b $i $region)"
 done
 
 # cmd="-in <(samtools view -u http://s3.amazonaws.com/1000genomes/data/HG00096/alignment/HG00096.chrom11.ILLUMINA.bwa.GBR.low_coverage.20111114.bam 11:108473-188673)"
