ff2a92b5caf69dc1fc702341a72f1603d3a94102
tonydisera <tony.disera@gmail.com>
2014-12-17 11:08:03 -0700

    Fix socket code so that email isn't sent until all data has been streamed.  Remove file after finished emailing.

--- services/emailServer.js
+++ services/emailServer.js
@@ -9,41 +9,66 @@ var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var server = BinaryServer({port: 7068});
 server.on('connection', function(client){
-  client.on('stream', function(stream, meta){
+    client.on('stream', function(stream, meta){
 
-    var attachment_file_name = "temp_email_attachment_" + new Date().toString() + '.txt';
+        var timestamp = new Date().getTime();
 
-    var file = fs.createWriteStream(attachment_file_name);
-    stream.pipe(file);
+        // Save the input stream to a file.  (Can't get nodemailer to work with the
+        // input stream for some reason.)
+        var attachment_file_name = './emailServer_attachment' + timestamp + '.txt';
+        var attachment_file = fs.createWriteStream(attachment_file_name);
+        stream.pipe(attachment_file);
 
-    var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'some.account@gmail.com',
-            pass: 'password_goes_here'
-        }
-    });
-    transporter.sendMail(
-        {
-            from:    meta.from,
-            to:      meta.to,
-            subject: meta.subject,
-            html:    meta.note,
-            attachments: [
-                {   
-                	filename: meta.filename,
-                    path: attachment_file_name
+        stream.on('data', function(data){
+        });
+
+        stream.on('end', function() {
+            // Configure nodemailer to use the gmail smtp server
+            var nodemailer = require('nodemailer');
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'someaccount@gmail.com',
+                    pass: 'enter_password_here'
                 }
-            ]
-        }, function(error, response){
-            if(error){
-                console.log(error);
-                return;
-            }
+            });
 
-           
-        }
-        );
-  	}); 
+            // Send the email.  Fill in the email address from, email address to,
+            // subject, note.  Attach the file that was streamed.
+            transporter.sendMail(
+                {
+                    from:    meta.from,
+                    to:      meta.to,
+                    subject: meta.subject,
+                    html:    meta.note,
+                    attachments: [
+                        {   
+                            filename: meta.filename,
+                            path: attachment_file_name
+                        }
+                    ]
+                }, function(error, response){
+                    // Now delete the termpory file
+                    fs.unlink(attachment_file_name, function (err) {
+                        if (err) {
+                            console.log("unable to delete temp file " + attachment_file_name);
+                            console.log(err.toString());
+                        } 
+                    });
+
+                    if(error){
+                        console.log(error);
+                        return;
+                    }
+                } 
+            );
+
+        });
+
+
+
+
+    });
+
+
 });
