a9eea6267366a3cf3e02040a4ead2acb537fba35
tonydisera <tony.disera@gmail.com>
2016-08-26 09:42:13 -0600

    Fix to use for emailing feedback.  It is now optional to send in an email attachment.

--- services/emailServer.js
+++ services/emailServer.js
@@ -2,7 +2,7 @@
 *  emailServer
 *  author:  Tony Di Sera, Dec 2014
 *  
-*  This server emails a note along with a file attachement (streamed to this server).
+*  This server emails a note along with a file attachment (streamed to this server).
 *
 */
 var BinaryServer = require('binaryjs').BinaryServer;
@@ -11,64 +11,70 @@ var server = BinaryServer({port: 7068});
 server.on('connection', function(client){
     client.on('stream', function(stream, meta){
 
-        var timestamp = new Date().getTime();
-
-        // Save the input stream to a file.  (Can't get nodemailer to work with the
-        // input stream for some reason.)
-        var attachment_file_name = './emailServer_attachment' + timestamp + '.txt';
-        var attachment_file = fs.createWriteStream(attachment_file_name);
-        stream.pipe(attachment_file);
-
-        stream.on('data', function(data){
-        });
-
-        stream.on('end', function() {
+        var sendEmail = function() {
             // Configure nodemailer to use the gmail smtp server
             var nodemailer = require('nodemailer');
             var transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
-                    user: 'your_support_email@gmail.com',
-                    pass: 'enter_password_here'
+                    user: 'support@somewhere.com',
+                    pass: 'password_goes_here'
                 }
             });
 
             // Send the email.  Fill in the email address from, email address to,
             // subject, note.  Attach the file that was streamed.
-            transporter.sendMail(
-                {
-                    from:    meta.from,
-                    to:      meta.to,
-                    subject: meta.subject,
-                    html:    meta.body,
-                    attachments: [
-                        {   
-                            filename: meta.filename,
-                            path: attachment_file_name
-                        }
-                    ]
-                }, function(error, response){
-                    // Now delete the termpory file
+            var emailObject = {
+                from:    meta.from,
+                to:      meta.to,
+                subject: meta.subject,
+                html:    meta.body
+            };
+            if (meta.filename) {
+                emailObject.attachments = [
+                    {   
+                        filename: meta.filename,
+                        path: attachment_file_name
+                    }
+                ];
+            }
+
+
+            transporter.sendMail( emailObject, function(error, response) {
+                if (meta.filename) {
+                    // Now delete the temporary file that is the attachment
                     fs.unlink(attachment_file_name, function (err) {
                         if (err) {
-                            console.log("unable to delete temp file " + attachment_file_name);
+                            console.log("emailServer.js: unable to delete temp file " + attachment_file_name);
                             console.log(err.toString());
                         } 
                     });
 
-                    if(error){
-                        console.log(error);
-                        return;
-                    }
-                } 
-            );
-
-        });
-
+                }
+                if(error){
+                    console.log(error);
+                    return;
+                }
+            });
+        };
+        
+        if (meta.filename) {
+            // Save the input stream to a file.  (Can't get nodemailer to work with the
+            // input stream for some reason.)
+            var timestamp = new Date().getTime();
+            var attachment_file_name = './emailServer_attachment' + timestamp + '.txt';
+            var attachment_file = fs.createWriteStream(attachment_file_name);
+            stream.pipe(attachment_file);
+            stream.on('data', function(data){
+            });
+            stream.on('end', function() {
+                sendEmail();
+            });            
+        } else {
+            sendEmail();
+        }
 
 
 
     });
-
-
 });
